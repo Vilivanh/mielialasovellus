c7df385a16cce40c7601844b4659dc943efc993d
Vili Vanhala <vili.vanhala@outlook.com>
2023-01-30 16:24:58 +0200

    tietokannat lisätty

--- tietokannat.sql
+++ tietokannat.sql
@@ -4,3 +4,21 @@ CREATE TABLE users (
 	    password TEXT
 );
 
+CREATE TABLE actions (
+	id SERIAL PRIMARY KEY,
+	name TEXT UNIQUE,
+	category TEXT, 
+	free BOOLEAN, 
+	lowcost BOOLEAN,
+	highcost BOOLEAN
+);
+
+CREATE TABLE effects (
+	id SERIAL PRIMARY KEY,
+        action_name TEXT,	
+	low BOOLEAN,
+	neutral BOOLEAN, 
+	high BOOLEAN	
+	sent_at TIMESTAMP
+);
+
