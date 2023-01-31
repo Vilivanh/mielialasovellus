64ad5edfba9ae9bd9c581a13626daac5640aa545
Vili Vanhala <vili.vanhala@outlook.com>
2023-01-31 10:23:51 +0200

    tietokantataujen luominen valmis

--- tietokannat.sql
+++ tietokannat.sql
@@ -18,7 +18,24 @@ CREATE TABLE effects (
         action_name TEXT,	
 	low BOOLEAN,
 	neutral BOOLEAN, 
-	high BOOLEAN	
+	high BOOLEAN,	
 	sent_at TIMESTAMP
 );
+
+CREATE TABLE private_actions (
+	id SERIAL PRIMARY KEY,
+	name TEXT,
+	category TEXT,
+	free BOOLEAN,
+	lowcost BOOLEAN,
+	highcost BOOLEAN, 
+	username TEXT
+);
+
+CREATE TABLE supervisors (
+	id SERIAL PRIMARY KEY,
+	username TEXT UNIQUE,
+	password TEXT
+);
+	
 
