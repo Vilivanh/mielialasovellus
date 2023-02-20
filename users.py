6ca0bb0da6c7e11b71e2c9af66e862e72bc1bf00
Vili Vanhala <vili.vanhala@outlook.com>
2023-02-20 20:20:00 +0200

    delete accidental secret key

--- users.py
+++ users.py
@@ -8,7 +8,7 @@ from os import getenv
  
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///vanhav"
+
 app.secret_key = getenv("SECRET_KEY")
 
 def login(username, password):
