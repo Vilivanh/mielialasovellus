3cc8df3457827411685f0ca0855695215af95166
Vili Vanhala <vili.vanhala@outlook.com>
2023-03-04 23:35:17 +0200

    remove unnecessary files

--- activitylist.py
+++
@@ -1,8 +0,0 @@
-def activitylist(chosen):
-    print '<table><tr><th>Column1</th><th>Column2</th>...</tr>'
-    for row in choser:
-        print '<tr>'
-        for col in row:
-            print '<td>%s</td>' % col
-            print '</tr>'
-            print '</table>'
